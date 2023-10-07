464f3e1a4b1d800af878f4dd6d6ce45ac1c66640
Adrien Gras <agr@owlnext.fr>
2023-10-08 01:54:45 +0200

    netlify patch

--- src/lib/libraries/google_auth.ts
+++ src/lib/libraries/google_auth.ts
@@ -1,12 +1,18 @@
 import { JWT } from "google-auth-library";
 import { env } from "$env/dynamic/private"
+import * as base64 from "base-64"
 
 export const JWTProvider = {
     async getJWT(scopes: string[]): Promise<JWT> {
+        const auth_sequence = env.GOOGLE_API_CREDS as string
+        const decoded = base64.decode(auth_sequence)
+
+        const creds = JSON.parse(decoded)
+
         return new Promise((resolve, reject) => {
             resolve(new JWT({
-                email: env.GOOGLE_AUTH_CLIENT_EMAIL,
-                key: env.GOOGLE_AUTH_PRIVATE_KEY,
+                email: creds.client_email,
+                key: creds.private_key,
                 scopes
             }))
         })
