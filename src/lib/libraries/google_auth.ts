64aac92f7fac22acc09fcfe4b963b07543378a0a
Adrien Gras <agr@owlnext.fr>
2023-10-08 01:19:34 +0200

    wip

--- src/lib/libraries/google_auth.ts
+++ src/lib/libraries/google_auth.ts
@@ -1,12 +1,12 @@
 import { JWT } from "google-auth-library";
-import creds from '../../../creds.json'
+import { env } from "$env/dynamic/private"
 
 export const JWTProvider = {
     async getJWT(scopes: string[]): Promise<JWT> {
         return new Promise((resolve, reject) => {
             resolve(new JWT({
-                email: creds.client_email,
-                key: creds.private_key,
+                email: env.GOOGLE_AUTH_CLIENT_EMAIL,
+                key: env.GOOGLE_AUTH_PRIVATE_KEY,
                 scopes
             }))
         })
